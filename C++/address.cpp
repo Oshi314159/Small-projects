a8774d216306ce00d99e8b76d48cc246b88ff228
Oshi314159 <ilsamo135135@gmail.com>
2026-07-26 09:30:37 +0900

    Using pointer and reference to return a review message

---
+++ C++/address.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+void reviewAction(const std::string& name, const std::string* extraComment) {
+    if (extraComment != nullptr && name != "") {
+        std::cout << "Thanks for the review, " << name << "! The team will get back to you if you had any questions."
+                  << std::endl;
+        return;
+    }
+    if (name != "") {
+        std::cout << "Thanks for the review, " << name << "!" << std::endl;
+        return;
+    }
+    return;
+}
+
+int main() {
+    reviewAction("", nullptr);
+}
